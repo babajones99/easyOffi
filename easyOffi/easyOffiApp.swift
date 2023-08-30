9c9ba8fae25fa70b5643cc0dcdbc178c104a2e33
babajones99 <50205747+babajones99@users.noreply.github.com>
2023-08-30 20:49:10 +0200

    Initial Commit

---
+++ easyOffi/easyOffiApp.swift
@@ -0,0 +1,17 @@
+//
+//  easyOffiApp.swift
+//  easyOffi
+//
+//  Created by Jonas Kunze on 30.08.23.
+//
+
+import SwiftUI
+
+@main
+struct easyOffiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
